1414baa7d51a7231cd1a72c6f40f6e79c7e538b2
CharaD7 <jijakahn6@gmail.com>
2023-12-22 20:12:38 +0000

    Got nav working

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,8 +9,20 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div className="bg-white py-6 lg:py-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div className="mb-10 md:mb-16">
+          <h2
+            className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl"
+          >
+            Top Trending Movies
+          </h2>
+        </div>
+
+        {/* Movie grid */}
+        <div></div>
+
+      </div>
     </div>
   );
 }
