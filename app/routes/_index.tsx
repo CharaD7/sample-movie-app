d07b79215e8a31a5877b6b490bcf72c166adec9d
CharaD7 <jijakahn6@gmail.com>
2023-12-22 21:33:00 +0000

    Load movie data from API

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,7 +8,24 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader({}: LoaderFunctionArgs) {
+  const url = await fetch(
+    'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+    {
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMDhlODlhNGM0ZTY1MzU1MTM4ZjIzOWMwNGE3NGI4NyIsInN1YiI6IjY1ODVlZmYzNDc3MjE1NWEwYjQzMTVlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SbO_hqJUwSCFfXS2cJbJP6vimpPrQHin6YKLihlBq3U'
+      }
+    }
+  );
+
+  return json(await url.json());
+}
+
 export default function Index() {
+  const data = useLoaderData();
+  console.log(data)
+
   return (
     <div className="bg-white py-6 lg:py-12">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
@@ -20,7 +38,8 @@ export default function Index() {
         </div>
 
         {/* Movie grid */}
-        <div></div>
+        <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8"></div>
+{/* eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMDhlODlhNGM0ZTY1MzU1MTM4ZjIzOWMwNGE3NGI4NyIsInN1YiI6IjY1ODVlZmYzNDc3MjE1NWEwYjQzMTVlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SbO_hqJUwSCFfXS2cJbJP6vimpPrQHin6YKLihlBq3U */}
 
       </div>
     </div>
