0a27cfe8457f9dc292d97fa79e4e9c7934be919a
CharaD7 <jijakahn6@gmail.com>
2023-12-22 19:59:08 +0000

    Removed obsolete futures

--- remix.config.js
+++ remix.config.js
@@ -7,12 +7,4 @@ export default {
   // serverBuildPath: "build/index.js",
   // server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
   tailwind: true,
-  future: {
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-    unstable_dev: true,
-  }
 };
