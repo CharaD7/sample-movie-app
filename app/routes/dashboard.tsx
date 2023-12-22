2735f3e08143cf954cd933208f48683f13dd1ecc
CharaD7 <jijakahn6@gmail.com>
2023-12-22 12:16:10 +0000

    Added the child route to parent Dashboard route

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "@remix-run/react";
+
 export default function Dashboad() {
   return (
     <div>
       <h1>Hello from the dashboard layout</h1>
+      <Outlet />
     </div>
   )
 }
