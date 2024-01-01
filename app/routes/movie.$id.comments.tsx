5d6d7e0cd11dcf35539860ed9ef22d7b744d907e
CharaD7 <jijakahn6@gmail.com>
2024-01-01 12:00:51 +0000

    Removed extra space

--- app/routes/movie.$id.comments.tsx
+++ app/routes/movie.$id.comments.tsx
@@ -59,7 +59,7 @@ export default function Comments() {
         <Form method="post" onSubmit={handleSubmit}>
           <textarea name="comment" value={comment} onChange={(e) => setComment(e.target.value)} className="w-full border border-teal-500 rounded-lg p-2"></textarea>
           <input type="hidden" name="id" value={id} />
-          
+
           { navigation.state === "submitting" ? (
             <button type="button" disabled className="bg-teal-500 px-4 py-2 rounded-lg text-white">Loading...</button>
           ) : (
