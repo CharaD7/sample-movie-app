998eae3c63b9b8e972d83ed29d5de6d0d36fa02e
CharaD7 <jijakahn6@gmail.com>
2024-01-01 11:52:29 +0000

    Added loading state to Add comment button

--- app/routes/movie.$id.comments.tsx
+++ app/routes/movie.$id.comments.tsx
@@ -1,5 +1,5 @@
 import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
-import { Form, useLoaderData, useParams } from "@remix-run/react";
+import { Form, useLoaderData, useNavigation, useParams } from "@remix-run/react";
 import { useState } from "react";
 import { db } from "~/utils/db.server";
 
@@ -49,6 +49,8 @@ export default function Comments() {
     setComment("");
   };
 
+  const navigation = useNavigation();
+
   return (
     <div className="rounded-lg border p-3">
       <h3 className="text-xl font-semibold mb-5">Your opinion</h3>
@@ -57,7 +59,13 @@ export default function Comments() {
         <Form method="post" onSubmit={handleSubmit}>
           <textarea name="comment" value={comment} onChange={(e) => setComment(e.target.value)} className="w-full border border-teal-500 rounded-lg p-2"></textarea>
           <input type="hidden" name="id" value={id} />
-          <button type="submit" className="bg-teal-500 px-4 py-2 rounded-lg text-white">Add Comment</button>
+          
+          { navigation.state === "submitting" ? (
+            <button type="button" disabled className="bg-teal-500 px-4 py-2 rounded-lg text-white">Loading...</button>
+          ) : (
+            <button type="submit" className="bg-teal-500 px-4 py-2 rounded-lg text-white">Add Comment</button>
+          )}
+
         </Form>
 
         <div className="mt-5 flex flex-col gap-y-3">
